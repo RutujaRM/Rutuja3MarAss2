b235e5ba085b9f8663434d31c8ac75a6e0dd4567
RutujaRM <96306705+RutujaRM@users.noreply.github.com>
2022-03-06 01:04:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+//JavaScript program to swap two variables
+
+
+let x = parseInt(prompt("Enter first number"))  //here we take first input from user
+let y = parseInt(prompt("Enter second number"))  //here we take second input from user
+
+x = x + y   // here add both the values of x and y
+y = x - y   // then we substract y value from total value then we get x value and that value assign to(y)
+x = x - y   // then we substract x value from total value then we get y value and that value assign to(x)
+
+console.log("After swap x is:  "+ x)
+console.log("After swap y is:  "+ y)
+
+document.write("After swap x is:   "+ x) ;
+document.write("<br>")
+document.write("<br>")
+document.write("<br>")
+document.write("After swap y is:   "+ y);
+   
+
+
